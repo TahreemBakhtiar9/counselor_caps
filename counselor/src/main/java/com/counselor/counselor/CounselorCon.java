25599d0720a74a739282bffbf7c43403946fd499
TahreemBakhtiar9 <tehreembakhtiar927@gmail.com>
2023-06-02 16:24:32 +0500

    update delete

--- counselor/src/main/java/com/counselor/counselor/CounselorCon.java
+++ counselor/src/main/java/com/counselor/counselor/CounselorCon.java
@@ -3,11 +3,17 @@ package com.counselor.counselor;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.persistence.Id;
 
 @RestController
 @RequestMapping("/counselor")
@@ -26,4 +32,15 @@ public class CounselorCon {
         this.counselorRepo.save(counselor);
     }
 
+    @PutMapping("/{id}")
+    public void updateCounselor(@RequestBody Counselor counselor){
+        this.counselorRepo.save(counselor);
+
+    }
+
+    @DeleteMapping("/{counselorId}")
+    public void deleteCounselor(@PathVariable Long counselorId){
+        this.counselorRepo.deleteById(counselorId);
+        
+    }
 }
